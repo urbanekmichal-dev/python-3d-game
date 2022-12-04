c525010d78e20f51c566d827b6a2589c8af1866b
Michal Urbanek <urbanekmichal.98@gmail.com>
2022-12-04 19:17:09 +0100

    add main game window

--- main.py
+++ main.py
@@ -19,4 +19,19 @@ class Game:
     def draw(self):
         self.screen.fill('black')
 
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+
     def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+        game = Game()
+        game.run()
